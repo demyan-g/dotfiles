95f9f2a246df71607ab73dc08c1f9c5595f4add0
demyan <bluets@gmail.com>
2018-08-17 18:35:25 +0900

    use-package ensure packages installed feature ADDED

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,18 +3,37 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+(elpy-enable)
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
 	("melpa" . "http://melpa.org/packages/")
 	("org" . "http://orgmode.org/elpa/")))
 
+;; Make sure <use-package> is avaiable
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; - Ensure packages installed
+(use-package magit :ensure t)
+(use-package elpy :ensure t)
+(use-package evil :ensure t)
+
 ;; Frame
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(display-time-mode +1)
 (when window-system (set-frame-size (selected-frame) 90 45))
 
 ;; Theme related
+(unless (package-installed-p 'zenburn-theme)
+  (package-install 'zenburn-theme))
+(unless (package-installed-p 'anti-zenburn-theme)
+  (package-install 'anti-zenburn-theme))
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
 
@@ -24,7 +43,7 @@
 (set-default-coding-systems 'utf-8)
 (set-default-font "Bitstream Vera Sans Mono 9")
 
-;; Indent without tab - just something
+;; Indent without tab
 (setq-default indent-tabs-mode nil)
 
 
@@ -38,7 +57,7 @@
     ("73c69e346ec1cb3d1508c2447f6518a6e582851792a8c0e57a22d6b9948071b4" "3f44e2d33b9deb2da947523e2169031d3707eec0426e78c7b8a646ef773a2077" default)))
  '(package-selected-packages
    (quote
-    (elpy use-package anti-zenburn-theme zenburn-theme company-statistics))))
+    (evil 2048-game evil-mode magit flymake-python-pyflakes elpy use-package anti-zenburn-theme zenburn-theme company-statistics))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

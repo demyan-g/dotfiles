b461e9655d1922785c34128fb967437e35d5b5a8
demyan <bluets@gmail.com>
2018-08-27 18:10:30 +0900

    python mode related features ADDED / font setting CHANGED to set-frame-font due to obsolete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,15 @@
+;;; package --- Summary
+;;; Commentary:
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+;;; Code:
 (package-initialize)
-(elpy-enable)
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-	("melpa" . "http://melpa.org/packages/")
-	("org" . "http://orgmode.org/elpa/")))
+        ("melpa" . "http://melpa.org/packages/")
+	    ("org" . "http://orgmode.org/elpa/")))
 
 ;; Make sure <use-package> is avaiable
 (unless (package-installed-p 'use-package)
@@ -19,15 +21,30 @@
 
 ;; - Ensure packages installed
 (use-package magit :ensure t)
-(use-package elpy :ensure t)
 (use-package evil :ensure t)
 (use-package yaml-mode :ensure t)
+(use-package elpy :ensure t)
+(use-package jedi :ensure t)
+(use-package flycheck :ensure t)
+(use-package yasnippet :ensure t)
+(use-package py-autopep8 :ensure t)
+(use-package helm :ensure t)
+(use-package helm-flycheck :ensure t)
+(use-package smartrep :ensure t)
+
+;; Yasnippet
+(yas-global-mode t)
+
+;; Flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
 
 ;; Frame
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (display-time-mode +1)
+(line-number-mode +1)
+(column-number-mode +1)
 (when window-system (set-frame-size (selected-frame) 90 45))
 
 ;; Theme related
@@ -42,11 +59,36 @@
 ;; - UTF-8 as default encoding
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
-(set-default-font "Bitstream Vera Sans Mono 9")
+(set-frame-font "Bitstream Vera Sans Mono 9")
 
 ;; Indent without tab
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
+
+;; Python-mode related
+(elpy-enable)
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python3" . python-mode))
+
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (define-key python-mode-map (kbd "\C-m") 'newline-and-indent)
+            (define-key python-mode-map (kbd "RET") 'newline-and-indent)))
+
+;; - Jedi setup
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq elpy-rpc-backend "jedi")
+(setq jedi:complete-on-dot t)
+
+(when (require 'flycheck nil t)
+  (remove-hook 'elpy-modules 'elpy-module-flymake)
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+(define-key elpy-mode-map (kbd "C-c C-v") 'helm-flycheck)
+(smartrep-define-key elpy-mode-map "C-c"
+                     '(("C-n" . flycheck-next-error)
+                       ("C-p" . flycheck-previous-error)))
 
 ;; YAML-mode related
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
@@ -64,12 +106,15 @@
  '(custom-safe-themes
    (quote
     ("73c69e346ec1cb3d1508c2447f6518a6e582851792a8c0e57a22d6b9948071b4" "3f44e2d33b9deb2da947523e2169031d3707eec0426e78c7b8a646ef773a2077" default)))
+ '(desktop-save-mode t)
  '(package-selected-packages
    (quote
-    (yaml-mode evil 2048-game evil-mode magit flymake-python-pyflakes elpy use-package anti-zenburn-theme zenburn-theme company-statistics))))
+    (python-outline smartrep helm-flycheck helm py-autopep8 flycheck company-jedi jedi yaml-mode evil 2048-game evil-mode magit flymake-python-pyflakes elpy use-package anti-zenburn-theme zenburn-theme company-statistics))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; init.el ends here
